63fefab2efa1fe3ebac5f6819f70f43bdca4d77c
DESKTOP-5836PEC\tiger <tiger980424@naver.com>
2022-03-01 13:39:34 +0900

    Kakao Code 요청 Url kakao controller에 주석으로 추가되어있음!

--- src/main/java/volume/controller/KakaoController.java
+++ src/main/java/volume/controller/KakaoController.java
@@ -14,6 +14,7 @@ public class KakaoController {
     private final KakaoService kakaoService;
 
     //https://kauth.kakao.com/oauth/authorize?client_id=f31b9f81fdf64d528eaafa1878d102c3&redirect_uri=http://localhost:8080/oauth/kakao&response_type=code
+    //https://kauth.kakao.com/oauth/authorize?client_id=f31b9f81fdf64d528eaafa1878d102c3&redirect_uri=https://volume-server-api.herokuapp.com/oauth/kakao&response_type=code
     @GetMapping("/oauth/kakao")
     public KakaoUserDTO kakaoLogin(@RequestParam String code) throws Exception{
         String access_Token = kakaoService.getKakaoAccessToken(code);
