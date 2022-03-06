9ed5c97bf9344b2056edd522c69c4e0b016d10a7
DESKTOP-5836PEC\tiger <tiger980424@naver.com>
2022-03-06 15:52:24 +0900

    Access-Control-Allow-Origin 추가

---
+++ src/main/java/volume/configuration/WebConfig.java
@@ -0,0 +1,15 @@
+package volume.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/api/**").allowedOrigins("http://localhost:3000");
+        registry.addMapping("/oauth/kakao").allowedOrigins("http://localhost:3000");
+    }
+}
